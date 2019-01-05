0bf4deee5ba6399d81fc9115468102cfb27396f1
Vazcore <vazcore@live.ru>
2019-01-06 00:03:11 +0300

    Created a StarterMap.

---
+++ Source/Test1Editor.Target.cs
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class Test1EditorTarget : TargetRules
+{
+	public Test1EditorTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+
+		ExtraModuleNames.AddRange( new string[] { "Test1" } );
+	}
+}
